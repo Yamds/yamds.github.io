0740765b341136c86fe0133256f909fe290340c5
Yamds <yamds@qq.com>
2025-02-23 19:53:50 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/', // 确保 base 配置正确
-  build: {
-    assetsDir: 'assets', // 静态资源目录
-  },
+  base: '/',
 }) 
